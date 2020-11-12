c9d1ab60d4a6b390a451844cfd958faf53269691
rkl-gilro <rgilrodr7@gmail.com>
2020-11-12 15:38:00 +0100

    Update jspsychsheet.js

--- jspsych-6.1.0/jspsychsheet.js
+++ jspsych-6.1.0/jspsychsheet.js
@@ -1,1 +1,53 @@
+// jsPsychSheet - A simple JavaScript library to use jsPsych and Google Sheet Apps Script to run behavioral experiments online.
+// Created by Shashi Kant Gupta, May 19, 2020.
 
+var jsPsychSheet = {
+  lastrow: 0,
+
+  showUploadStatus: function(){
+    var jspsych_content = document.getElementById("jspsych-content");
+    jspsych_content.innerHTML = 'Uploading your data<br><br><div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>'
+  },
+
+  onUploadSuccess: function(){
+    var jspsych_content = document.getElementById("jspsych-content");
+    jspsych_content.innerHTML = 'Your data is successfully uploaded!'
+  },
+
+  uploadData: function(all_data, inbetween=0){
+    if (inbetween == 0){
+      this.showUploadStatus();
+    }
+
+    var data_rows = all_data.split(/\r?\n|\r/);
+
+    var i;
+    var data = [];
+    var rowlast;
+    for(i=this.lastrow; i<data_rows.length; i++){
+      var row = data_rows[i];
+      row = row.slice(1, row.length-1)
+      row = row.split('","');
+      if(row.length > 1){
+        rowlast = i
+        data.push(row);
+      }
+    }
+
+    if (inbetween == 0){
+      if (this.lastrow == 0){
+        google.script.run.withSuccessHandler(this.onUploadSuccess).addData(data);
+      } else{
+        google.script.run.withSuccessHandler(this.onUploadSuccess).addDataMultiBlock(data);
+      }
+    } else {
+      if (this.lastrow == 0){
+        google.script.run.addDataMultiBlock(data, 1);
+      } else{
+        google.script.run.addDataMultiBlock(data);
+      }
+    }
+
+    this.lastrow = rowlast+1;
+  }
+}
